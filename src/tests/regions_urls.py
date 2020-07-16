77e44e0c86233b589006fc61c890bfc70404c2c1
Kate <e.malevannaya@index.ru>
2020-07-16 18:54:26 +0300

    Добавлены региональные сайты

--- src/tests/regions_urls.py
+++ src/tests/regions_urls.py
@@ -1,6 +1,16 @@
 # Ключ: Имя региона
 # Значение: Url
 regions = {
-    #"Пермский край": "https://k-vrachu.ru/",
+    "Пермский край": "https://k-vrachu.ru/",
     "Астраханская область": "https://doctor30.ru/",
+    "Вологодская область": "https://k-vrachu.volmed.org.ru/",
+    "Калужская область": "https://kaluga_new.k-vrachu.ru",
+    "Пензенская область": "https://пенза-доктор.рф",
+    "Псковская область": "https://k-vrachu.pskov.ru/",
+    "Республика Башкортостан": "http://doctor.bashkortostan.ru",
+    "Республика Бурятия": "http://reg03.k-vrachu.ru",
+    "Республика Карелия": "http://reg.zdrav10.ru",
+    "Республика Крым": "https://crimea.k-vrachu.ru/",
+    "Республика Хакасия": "https://кврачу19.рф/",
+    "Свердловская область": "https://so.k-vrachu.ru/"
 }
