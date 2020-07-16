3d0d8f19a97b98b8644071df9f056057f9472d4a
Kate <e.malevannaya@index.ru>
2020-07-16 18:55:11 +0300

    Исправлен формат кода

--- src/tests/features/demo_mode/test_demo_register_to_doctor.py
+++ src/tests/features/demo_mode/test_demo_register_to_doctor.py
@@ -1,50 +1,49 @@
 """Demo mode feature tests."""
-
-from src.pages.KvrachuPages import (MainPage, DemoPage)
+from pytest_html import extras
 from src.tests.regions_urls import *
+from pytest_bdd import (given, scenario, then, when)
+from src.pages.KvrachuPages import (MainPage, DemoPage)
 from selenium.webdriver.support.wait import TimeoutException
-from pytest_bdd import (
-    given,
-    scenario,
-    then,
-    when,
-)
 
+# Константы
 REGISTER_ERROR_MESSAGE = "Запись в базу данных невозможна"
 
+# Сценарий
 
 @scenario('demo_register_to_doctor.feature', 'Register to doctor from demo account')
 def test_register_to_doctor_from_demo_account():
     """Register to doctor from demo account."""
     pass
 
+# Шаги
 
 @given('user entered demo mode')
 def user_entered_demo_mode(driver):
-    """user entered demo mode."""
     success = False
     main_page = MainPage(driver)
 
     for region, url in regions.items():
         try:
-            # 1. поиск кнопки с демо режимомом
+            # 1. Поиск кнопки с демо режимомом
             main_page.go_to_site(url)
 
-            # 2. вход в демо режим
+            # 2. Вход в демо режим
             main_page.to_demo_page()
 
-            # 3. проверяем что демо режим включен
+            # 3. Проверяем что демо режим включен
             main_page.get_profile_element()
 
             # Регион найден
             success = True
             break
-        # в случае ошибки на первых шагах -> повторить с шага 1
+        # В случае ошибки на первых шагах -> повторить с шага 1
         except TimeoutException:
+            print('')
             print(f'В регионе {region} не доступен работающий демо режим')
             continue
-        # в случае непредвиденной ошибки выходим и валим тест
+        # В случае непредвиденной ошибки выходим и валим тест
         except BaseException as e:
+            print('')
             print(f'Ошибка при поиске региона с работающим демо режимом. {e}')
             break
 
@@ -54,7 +53,6 @@ def user_entered_demo_mode(driver):
 
 @when('user selects the registration service')
 def user_selects_the_registration_service(driver):
-    """user selects the registration service."""
     demo_page = DemoPage(driver)
     demo_page.register_to_doctor()
 
